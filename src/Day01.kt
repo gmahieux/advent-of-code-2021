daff3939e67471b6806b6c72c1dae8f4dd962c83
Ghislain MAHIEUX <ghislain.mahieux@sundayapp.com>
2021-12-01 22:37:40 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,16 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<Int>): Int = input.windowed(2).count { (prev, cur) -> cur > prev }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<Int>): Int = input
+        .windowed(size = 3) { it.sum() }
+        .windowed(2).count { (prev, cur) -> cur > prev }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput.toInts()) == 7)
+    check(part2(testInput.toInts()) == 5)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input.toInts()))
+    println(part2(input.toInts()))
 }
